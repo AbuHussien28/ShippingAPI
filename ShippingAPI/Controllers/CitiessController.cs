778045c024b4eb466794f0e1eb37042b2826cc9e
Hager172 <hagerayman737@gmail.com>
2025-06-26 15:34:45 +0300

    first commit

---
+++ ShippingAPI/Controllers/CitiessController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShippingAPI.UnitOfWorks;
+
+namespace ShippingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiessController : ControllerBase
+    {
+        private readonly UnitOfWork uow;
+
+       public CitiessController(UnitOfWork Uow)
+        {
+            uow = Uow;
+        }
+
+        public IActionResult GetCities()
+        {
+            var cities = uow.CityRepo.getAll();
+            return Ok(cities);
+        }
+    }
+}
